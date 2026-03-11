980abfb9b75cf9401e6f09128aab7c61f683fb23
SentamilselvanA <selvanasentamil@gmail.com>
2026-03-11 10:44:24 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://dev-track-74es.onrender.com/api"
 });
 
 instance.interceptors.request.use((config) => {
